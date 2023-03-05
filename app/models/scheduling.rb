8101b694c2709aa6fb697f3fd9cdc8a0993fd646
EC2 Default User <ec2-user@ip-172-31-1-42.ap-northeast-1.compute.internal>
2023-03-05 11:06:03 +0000

    エラー箇所を修正(route.rbにミススペルがありました)

--- app/models/scheduling.rb
+++ app/models/scheduling.rb
@@ -1,2 +1,14 @@
 class Scheduling < ApplicationRecord
+  
+  validates :title, presence:true, length: {maximum:20}
+  validates :start_date, presence:true
+  validates :end_date, presence:true
+  validates :memo, length: {maximum:500}
+  validate :date_check
+
+  def date_check
+    return if start_date.blank? || end_date.blank?
+    errors.add(:end_date, "(終了日)は開始日以降のものを選択してください" )if end_date < start_date
+  end
+  
 end
